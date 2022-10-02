8b2fcef4c30972f27ba87c9c0a848474f758af5e
Rey  Baterbonia <poochinng@gmail.com>
2022-10-02 21:16:43 +0800

    init

---
+++ script.js
@@ -0,0 +1,42 @@
+function myFunction1(){
+    document.getElementById('info1').style.display = "flex";
+}
+
+function myFunction2(){
+    document.getElementById('info1').style.display = "none";
+}
+function myFunction3(){
+    document.getElementById('info2').style.display = "flex";
+}
+
+function myFunction4(){
+    document.getElementById('info2').style.display = "none";
+}
+function myFunction5(){
+    document.getElementById('info3').style.display = "flex";
+}
+
+function myFunction6(){
+    document.getElementById('info3').style.display = "none";
+}
+function myFunction7(){
+    document.getElementById('info4').style.display = "flex";
+}
+
+function myFunction8(){
+    document.getElementById('info4').style.display = "none";
+}
+function myFunction9(){
+    document.getElementById('info5').style.display = "flex";
+}
+
+function myFunction10(){
+    document.getElementById('info5').style.display = "none";
+}
+function myFunction11(){
+    document.getElementById('info6').style.display = "flex";
+}
+
+function myFunction12(){
+    document.getElementById('info6').style.display = "none";
+}
