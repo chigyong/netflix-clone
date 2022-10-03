e256214e1060b66c80f6c7eba43fd59bb3fca7fd
Rey  Baterbonia <poochinng@gmail.com>
2022-10-03 16:53:10 +0800

    init

---
+++ script.js
@@ -0,0 +1,61 @@
+function openInfo(){
+    var open = document.getElementById('info');
+    if(open.style.display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+
+    }
+}
+function openInfo1(){
+    var open = document.getElementById('info1');
+    if(open.style.display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+
+    }
+}
+function openInfo2(){
+    var open = document.getElementById('info2');
+    if(open.style.display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+
+    }
+}
+function openInfo3(){
+    var open = document.getElementById('info3');
+    if(open.style.display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+
+    }
+}
+function openInfo4(){
+    var open = document.getElementById('info4');
+    if(open.style.display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+
+    }
+}
+function openInfo5(){
+    var open = document.getElementById('info5');
+    if(open.style.display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+
+    }
+}
+
